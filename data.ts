b8bb1a0d519074b49bf68840cc928ac8c114856e
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-16 21:54:31 +0800

    Add typedefs, query and resolvers, Initial queries and data

---
+++ data.ts
@@ -0,0 +1,5 @@
+import provinces from "./data/provinces";
+import regions from "./data/regions";
+import cities from "./data/cities";
+
+export { provinces, regions, cities };
--- index.ts
+++ index.ts
@@ -1,1 +1,45 @@
+import { ApolloServer, gql } from "apollo-server";
+import { regions, provinces, cities } from "./data";
 
+const typeDefs = gql`
+  type Region {
+    psgcCode: String
+    regionName: String
+    regionCode: String
+    numOfProvinces: Int
+    numOfMunicipalities: Int
+    numOfBrgys: Int
+  }
+
+  type Province {
+    provinceCode: String
+    provinceName: String
+    psgcCode: String
+    regionCode: String
+  }
+
+  type City {
+    cityCode: String
+    cityName: String
+    provinceCode: String
+    psgcCode: String
+    regionCode: String
+  }
+
+  type Query {
+    regions: [Region]
+    provinces: [Province]
+    cities: [City]
+  }
+`;
+
+const resolvers = {
+  Query: {
+    regions: () => regions,
+    provinces: () => provinces,
+    cities: () => cities,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.listen().then(({ url }) => console.log("🚀 Server is ready at", url));
