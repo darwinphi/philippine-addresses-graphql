f396060f1bcddefabcf3f3a629c7c249403023cf
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-17 20:00:57 +0800

    Add local dev, Refactor

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,16 @@
-import { ApolloServer } from "@saeris/apollo-server-vercel";
 import { regions, provinces, cities } from "./data";
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http, { Server } from "http";
+import express, { Express } from "express";
+import cors from "cors";
 
-const typeDefs = `
+const app: Express = express();
+app.use(cors());
+app.use(express.json());
+const httpServer: Server = http.createServer(app);
+
+const typeDefs = gql`
   type Region {
     psgcCode: String
     regionName: String
@@ -41,10 +50,18 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  playground: true,
-  introspection: true,
-});
-export default server.createHandler();
+const startApolloServer = async (app: Express, httpServer: Server) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    introspection: true,
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import app from "../api/index";
+
+app.listen(4000, () =>
+  console.log("🚀 Server ready at http://localhost:4000/")
+);
