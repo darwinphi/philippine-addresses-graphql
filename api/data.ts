60b9e19d31abadf382aea3fbb9ed736a42326ce3
Darwin Manalo <darwinmanalophi@gmail.com>
2022-02-19 12:08:23 +0800

    Use readfile

--- api/data.ts
+++ api/data.ts
@@ -1,6 +1,31 @@
+import fs from "fs";
+import path from "path";
 import provinces from "./data/provinces";
 import regions from "./data/regions";
-import { default as cities } from "./data/cities.json";
-import { default as barangays } from "./data/barangays.json";
+
+interface City {
+  cityCode: string;
+  cityName: string;
+  provinceCode: string;
+  psgcCode: string;
+  regionCode: string;
+}
+
+interface Barangay {
+  brgyCode: string;
+  brgyName: string;
+  cityCode: string;
+  provinceCode: string;
+  regionCode: string;
+}
+
+const getData = (filename: string) => {
+  const jsonPath = path.join(__dirname, "data", filename);
+  const result = fs.readFileSync(jsonPath, "utf-8");
+  return JSON.parse(result);
+};
+
+const barangays: Barangay[] = getData("barangays.json");
+const cities: City[] = getData("cities.json");
 
 export { provinces, regions, cities, barangays };
